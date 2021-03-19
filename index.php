7025ee04fc702480d8e1a365f5342568ec8682ca
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-03-19 22:07:37 +0200

    validation for empty entries

--- index.php
+++ index.php
@@ -63,20 +63,24 @@
         // Add employee/project logic
 
         if (isset($_POST['create'])) {
+            $fname = $_POST['fname'];
+            $a_project = $_POST['a_proj'];
+
+            $title = $_POST['new_project'];
+
             if ($_GET['path'] == 'employees/') {
                 $stmt = $conn->prepare("INSERT INTO empl (fname, assigned_project) VALUES (?, ?)");
                 $stmt->bind_param("ss", $fname, $a_project);
-                $fname = $_POST['fname'];
-                $a_project = $_POST['a_proj'];
             } else {
                 $stmt = $conn->prepare("INSERT INTO proj (title) VALUES (?)");
                 $stmt->bind_param("s", $title);
-                $title = $_POST['new_project'];
             }
-            $stmt->execute();
-            $stmt->close();
-            header('Location: ' . $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']);
-            die;
+            if (!empty($fname) || !empty($title)) {
+                $stmt->execute();
+                $stmt->close();
+                header('Location: ' . $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']);
+                die;
+            }
         }
 
         if ($_GET['path'] == 'employees/') {
